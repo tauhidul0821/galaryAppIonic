b8cfc1aab84861506341c49cf95b0027e5e2621a
Tauhidul Islam Tuhin <abdurshahin5@gmail.com>
2019-11-18 18:59:50 +0600

    first commit

---
+++ src/app/gallery/gallery.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: 'gallery.page.html',
+  styleUrls: ['gallery.page.scss']
+})
+export class GalleryPage {
+
+  constructor() {}
+
+}
